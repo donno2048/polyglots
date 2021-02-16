20dc9bcd332c35380daf2fd523335cac967d6cf6
Elisha Hollander <just4now666666@gmail.com>
2021-02-16 18:48:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 setup(
     name='polyglots',
-    version='1.0.1',
+    version='1.0.2',
     description='Create polyglots for different file types',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
@@ -10,4 +10,5 @@ setup(
     license='MIT',
     author='Elisha Hollander',
     classifiers=['Programming Language :: Python :: 3'],
+    entry_points={ 'console_scripts': [ 'polyglots=polyglots.__main__:main' ] }
 )
