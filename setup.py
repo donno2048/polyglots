a270ffa7aefc0ca285e96c7c7eb26aadf21c6e40
Elisha Hollander <just4now666666@gmail.com>
2021-02-18 00:29:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 setup(
     name='polyglots',
-    version='1.0.4',
+    version='1.0.5',
     description='Create polyglots for different file types',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
